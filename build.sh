d6500240c09987a8c0713a8c7ca1adef1048c121
sric <lzqiong@gmail.com>
2024-10-07 16:09:14 +0800

    bdist upload

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker run --rm -v "$(pwd)":/app -it python:3.9.19-bookworm bash -c "cd /app && python -m pip install --upgrade pip setuptools wheel numpy && python setup.py bdist_wheel"
---
+++ upload_pypi.sh
@@ -0,0 +1,2 @@
+# install twine: python3 -m pip install --upgrade twine
+twine upload --repository-url http://127.0.0.1:7007/ dist/*
