7069563d370aebb598baceb8227b15fb83493458
sric0880 <justgotpaid88@qq.com>
2025-01-22 22:54:59 +0800

    add pydt_from_millisecond and pydt_from_microsecond

--- quantcalendar/utils_datetime.py
+++ quantcalendar/utils_datetime.py
@@ -1,7 +1,7 @@
 import time
 from datetime import datetime
 
-from numpy import datetime64
+# from numpy import datetime64
 
 
 def pydt_from_second(sec: int) -> datetime:
@@ -13,6 +13,26 @@ def pydt_from_second(sec: int) -> datetime:
     return datetime(y, m, d, hh, mm, ss, 0)
 
 
+def pydt_from_millisecond(milli: int) -> datetime:
+    """
+    Convert native timestamp to local naive datetime
+    """
+    sec, millis = divmod(milli, 1000)
+    y, m, d, hh, mm, ss, weekday, jday, dst = time.gmtime(sec)
+    ss = min(ss, 59)  # clamp out leap seconds if the platform has them
+    return datetime(y, m, d, hh, mm, ss, millis * 1000)
+
+
+def pydt_from_microsecond(micro: int) -> datetime:
+    """
+    Convert native timestamp to local naive datetime
+    """
+    sec, micros = divmod(micro, 1000000)
+    y, m, d, hh, mm, ss, weekday, jday, dst = time.gmtime(sec)
+    ss = min(ss, 59)  # clamp out leap seconds if the platform has them
+    return datetime(y, m, d, hh, mm, ss, micros)
+
+
 def pydt_from_sec_list(seconds: list[int]) -> list[datetime]:
     """
     Convert list of native timestamp to list of local naive datetime
@@ -20,15 +40,15 @@ def pydt_from_sec_list(seconds: list[int]) -> list[datetime]:
     return [pydt_from_second(sec) for sec in seconds]
 
 
-def npydt_from_second(sec: int) -> datetime64:
-    """
-    Convert to a datetime64 representation of native timestamp
-    """
-    return datetime64(sec, "s")
+# def npydt_from_second(sec: int) -> datetime64:
+#     """
+#     Convert to a datetime64 representation of native timestamp
+#     """
+#     return datetime64(sec, "s")
 
 
-def npydt_from_sec_list(seconds: list[int]) -> list[datetime64]:
-    """
-    Convert to datetime64 representation of native timestamp in list
-    """
-    return [datetime64(sec, "s") for sec in seconds]
+# def npydt_from_sec_list(seconds: list[int]) -> list[datetime64]:
+#     """
+#     Convert to datetime64 representation of native timestamp in list
+#     """
+#     return [datetime64(sec, "s") for sec in seconds]
