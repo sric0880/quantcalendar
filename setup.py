517e9d1eb4554979049c1751262ede498d8113b7
qiong <lzqiong@gmail.com>
2025-09-15 14:50:38 +0800

    version changed to 2025.09

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="quantcalendar",
-    version="2025.06",
+    version="2025.09",
     author="lzq",
     description="trade calendar",
     license="MIT",
